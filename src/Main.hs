d092f364e3e1108646e18a84c1d08de2135e99d3
Chris Tetreault <chris@doingmyprogramming.com>
2015-07-13 12:36:45 -0700

    replaced old main with new main
    new main actually works. Old kept for posterity

--- src/Main.hs
+++ src/Main.hs
@@ -1,74 +1,58 @@
-{-|
-Module : Main
-Description : Graph Solver
-License : GPL-3
-Stability : probably not very
-
-This module is a simple graph solver. Nodes can have dependencies, and use
-a "solver" function to "solve" them, once their dependencies are satisfied.
--}
-
-module Main where
-
+import Criterion.Main
+import Control.Concurrent
+import System.Process
+import Control.Monad
+import Control.Parallel.Strategies
 import qualified Data.Map as M
 import Data.Graph
 import Data.List
-import Data.Maybe
-import Control.Monad
-
--- conSolveFn
-import Control.Concurrent
-
--- parSolveFn
-import Control.Parallel.Strategies
-
--- Imports for testing
-import Criterion.Main
 import System.Random
-import System.Process
+
+-- TYPES
+
+data Workers a b =
+   Workers
+   {toThread :: Chan a,
+    fromThread :: Chan b}
 
 -- | A triple of a node, its key, and the keys of its dependencies
 type Node node = (node, Int, [Int])
 
 -- | The solver input
 data Input node soln =
-   Input {solver :: node -> [soln] -> IO soln,
-          getNode :: Vertex -> Node node,
+   Input {getNode :: Vertex -> Node node,
           getVertex :: Int -> Maybe Vertex,
           graph :: Graph}
 
 -- | Construct an Input
-input :: (node ->[soln] -> IO soln) -- ^ The solver function.
-         -> [Node node] -- ^ A node, a unique identifier for the node
+input :: [Node node] -- ^ A node, a unique identifier for the node
          -- and a list of node IDs upon which this node depends.
          -> Input node soln -- ^ A new input object
-input s g =
-   Input {solver = s,
-          getNode = getter,
+input g =
+   Input {getNode = getter,
           getVertex = vGetter,
           graph = inGraph}
    where (inGraph, getter, vGetter) = graphFromEdges g
 
--- | Gets a node from the graph by key, and the nodes it depends on
-getVal :: Input node soln -- ^ The graph to access
-          -> Int -- ^ The key corresponding to the node
-          -> Maybe (node, [Int]) -- ^ A pair of a node, and the keys
-          -- of its dependencies, or Nothing if the node isn't in the graph
-getVal i k = do
-   vertex <- getVertex i k
-   (node, _, deps) <- return $ getNode i vertex
-   return (node, deps)
-
 -- | The final output of the solver. A map from keys to solutions
 type Output soln = M.Map Int soln
+
+-- SOLVING FUNCTIONS
 
 -- | Get a list of nodes that are ready to be solved
 readyNodes :: Input node soln -- ^ The input
               -> Output soln -- ^ The current built up solution map
-              -> [Node node] -- ^ A list of nodes who's dependencies
-              -- have all been solved
-readyNodes i o = filter readyNode nkdList
+              -> [(Node node, [soln])] -- ^ A list of nodes who's dependencies
+              -- have all been solved, paired with their solutions
+readyNodes i o = map fromJust'
+                 $ filter dropNothing
+                 $ map pairSoln
+                 $ filter readyNode nkdList
    where
+      fromJust' (n, Just s) = (n, s)
+      dropNothing (_, Nothing) = False
+      dropNothing _ = True
+      pairSoln n = (n, getSolutions n o)
       verts = vertices (graph i)
       nkdList = map (getNode i) verts
       readyNode (_, k', d') =
@@ -84,37 +68,6 @@ getSolutions (_, _, d) o = do
    mSolns <- return $ map (`M.lookup` o) d
    sequence mSolns
 
--- | Solve the input and produce an output map. Solves batches of ready
--- nodes using the batch solver function
-solve :: Input node soln -- ^ The input to solve
-         -> ([Node node]
-             -> Output soln
-             -> (node -> [soln] -> IO soln)
-             -> IO (Output soln)) -- ^ A function to solve a batch of nodes
-         -> IO (Output soln) -- ^ An IO action containing the solutions
-solve i f = solve' M.empty
-   where
-      solve' o' = do
-         nodes <- return $ readyNodes i o'
-         if null nodes
-            then
-            return o'
-            else
-            do
-               o'' <- f nodes o' (solver i)
-               solve' o''
-
--- | Attempts to solve a node. Fails if the provided solution map isn't complete
-trySolve :: Node node -- ^ The node to try
-            -> Maybe [soln] -- ^ Potentially a list of solutions for this node
-            -> (node -> [soln] -> IO soln) -- ^ The solver function
-            -> IO (Maybe (Int, soln)) -- ^ A solution, and its key, or Nothing
-            -- if the prerequisite solution list was incomplete
-trySolve _ Nothing _ = return Nothing
-trySolve (n, k, _) (Just s) f = do
-   soln <- f n s
-   return $ Just (k, soln)
-
 -- | Adds a list of key/solution pairs to a solution map
 addAll :: Output soln -- ^ The solution map
           -> [(Int, soln)] -- ^ The new solutions to add
@@ -122,207 +75,79 @@ addAll :: Output soln -- ^ The solution map
 addAll o [] = o
 addAll o ((k, s):xs) = addAll (M.insert k s o) xs
 
--- | Solves a list of nodes serially
-serSolveFn :: [Node node] -- ^ The nodes to solve
-              -> Output soln -- ^ The current built-up solution map
-              -> (node -> [soln] -> IO soln) -- ^ The solver function
-              -> IO (Output soln) -- ^ The updated solution map
-serSolveFn [] o _ = return o
-serSolveFn n o f = do
-   mRes <- mapM (\a -> trySolve a (getSolutions a o) f) n
-   res <- return $ map fromJust $ filter isJust mRes
-   return $ addAll o res
+initWorkers :: (Workers a b -> IO ()) -> Int -> IO (Workers a b)
+initWorkers a n = do
+   tx <- newChan
+   rx <- newChan
+   let workers = Workers {toThread = tx, fromThread = rx}
+   let actions =  replicate n $ a workers
+   mapM_ forkIO actions
+   return workers
 
--- | Solves a list of nodes in parallel using Control.Parallel
-parSolveFn :: (NFData soln)
-              => [Node node] -- ^ The nodes to solve
-              -> Output soln -- ^ The current built-up solution map
-              -> (node -> [soln] -> IO soln) -- ^ The solver function
-              -> IO (Output soln) -- ^ The updated solution map
-parSolveFn [] o _ = return o
-parSolveFn n o f = do
-   mRes <- mapM mapTrySolve n
-   res <- return $ map fromJust $ filter isJust mRes
-   return $ addAll o res
+--action :: (Int, Int) -> IO (Int, Float)
+action :: (Node Int, [Float]) -> IO (Int, Float)
+action ((n, k, _), s) = do
+   let plusOrMinusFive = (0.01 :: Float) * fromIntegral (4 + (n `mod` 3))
+   _ <- system $ "sleep " ++ show plusOrMinusFive
+   let useSolns = (foldl' (+) 0 s)
+   return $ (k, plusOrMinusFive + useSolns)
+
+--workerAction :: Workers (Int, Int) (Int, Float) -> IO ()
+workerAction w = forever $ do
+   arg <- readChan $ toThread w
+   res <- action arg
+   writeChan (fromThread w) res
+
+--serSolve :: [(Int, Int)] -> IO [(Int, Float)]
+serSolve = mapM action
+
+--parSolve :: Workers (Int, Int) (Int, Float) -> [(Int, Int)] -> IO [(Int, Float)]
+parSolve :: Workers (Node node, [soln]) (Int, soln)
+            -> [(Node node, [soln])]
+            -> IO [(Int, soln)]
+parSolve w i = do
+   writeList2Chan (toThread w) i
+   waitForAll (length i) []
    where
-      mapTrySolve a = runEval $ rseq $ trySolve a (getSolutions a o) f
+      waitForAll 0 o = sequence o
+      waitForAll n o = waitForAll (n - 1) (readChan (fromThread w) : o)
 
-data ToThread node soln = Destroy
-                        | Solve (Node node) (Maybe [soln])
-
-data FromThread soln = ThreadDeath
-                     | Result (IO (Maybe (Int, soln)))
-
--- | Solves a list of nodes in parallel using Control.Concurrent
-conSolveFn :: Int -- ^ The number of threads to spawn
-              -> [Node node] -- ^ The nodes to solve
-              -> Output soln -- ^ The current built-up solution map
-              -> (node -> [soln] -> IO soln) -- ^ The solver function
-              -> IO (Output soln) -- ^ The updated solution map
-conSolveFn _ [] o _ = return o
-conSolveFn nt n o f = do
-   tt <- newChan
-   ft <- newChan
-   taList <- return $ replicate nt $ threadAction tt ft f
-   mapM_ forkIO taList
-   solveList <- return $ map toToThread n
-   writeList2Chan tt solveList
-   writeList2Chan tt $ replicate nt Destroy
-   running nt [] ft
+solve :: ([(Node node, [soln])] -> IO [(Int, soln)])
+         -> Input node soln
+         -> IO (Output soln)
+solve f i = solve' M.empty
    where
-      running 0 resList _ = do
-         mRes <- mapM (\(Result r) -> r) resList
-         res <- return $ map fromJust $ filter isJust mRes
-         return $ addAll o res
-      running c resList ft' = do
-         curr <- readChan ft'
-         case curr of
-            ThreadDeath -> running (c - 1) resList ft'
-            result -> running c (result:resList) ft'
-      toToThread n' = Solve n' (getSolutions n' o)
+      solve' o = do
+         nodes <- return $ readyNodes i o
+         if null nodes
+            then
+            return o
+            else
+            do
+               o' <- f nodes
+               solve' $ addAll o o'
 
-
-threadAction tt' ft' f = do
-   val <- readChan tt'
-   case val of
-      Destroy -> writeChan ft' ThreadDeath
-      (Solve n' s) -> do
-         writeChan ft' $ Result $! trySolve n' s f
-         threadAction tt' ft' f
-
-initWorkers :: Int
-               -> (node -> [soln] -> IO soln)
-               -> IO (Chan (ToThread node soln), Chan (FromThread soln))
-initWorkers nt f = do
-   tt <- newChan
-   ft <- newChan
-   taList <- return $ replicate nt $ threadAction tt ft f
-   mapM_ forkIO taList
-   return (tt, ft)
-
--- | Solves a list of nodes in parallel using Control.Concurrent
-wkrSolveFn :: (Chan (ToThread node soln), Chan (FromThread soln))
-              -> [Node node] -- ^ The nodes to solve
-              -> Output soln -- ^ The current built-up solution map
-              -> (node -> [soln] -> IO soln) -- ^ The solver function
-              -> IO (Output soln) -- ^ The updated solution map
-wkrSolveFn _ [] o _ = return o
-wkrSolveFn (tt, ft) n o _ = do
-   solveList <- return $ map toToThread n
-   writeList2Chan tt solveList
-   running (length n) [] ft
-   where
-      running 0 resList _ = do
-         mRes <- mapM (\(Result r) -> r) resList
-         res <- return $ map fromJust $ filter isJust mRes
-         return $ addAll o res
-      running c resList ft' = do
-         curr <- readChan ft'
-         case curr of
-            ThreadDeath -> running c resList ft'
-            result -> running (c - 1) (result:resList) ft'
-      toToThread n' = Solve n' (getSolutions n' o)
-
--- | main test harness
-main :: IO ()
 main = do
-   threadCount <- getNumCapabilities
-   txrx <- initWorkers threadCount sFn_dmesg
-   defaultMain [bgroup "dmesg"
-                [bgroup "2x4"
-                 [bench "serial" $ nfIO
-                  $ solve (testInput 2 4) serSolveFn,
-                  bench "parallel" $ nfIO
-                  $ solve (testInput 2 4) parSolveFn,
-                  bench "concurrent" $ nfIO
-                  $ solve (testInput 2 4) (conSolveFn threadCount),
-                  bench "workers" $ nfIO
-                  $ solve (testInput 2 4) (wkrSolveFn txrx)],
-                 bgroup "8x4"
-                 [bench "serial" $ nfIO
-                  $ solve (testInput 8 4) serSolveFn,
-                  bench "parallel" $ nfIO
-                  $ solve (testInput 8 4) parSolveFn,
-                  bench "concurrent" $ nfIO
-                  $ solve (testInput 8 4) (conSolveFn threadCount),
-                  bench "workers" $ nfIO
-                  $ solve (testInput 8 4) (wkrSolveFn txrx)],
-                 bgroup "16x4"
-                 [bench "serial" $ nfIO
-                  $ solve (testInput 16 4) serSolveFn,
-                  bench "parallel" $ nfIO
-                  $ solve (testInput 16 4) parSolveFn,
-                  bench "concurrent" $ nfIO
-                  $ solve (testInput 16 4) (conSolveFn threadCount),
-                  bench "workers" $ nfIO
-                  $ solve (testInput 16 4) (wkrSolveFn txrx)]
+   let rand = mkStdGen 1337
+   let testTen =
+          runEval $ evalList rdeepseq $ testGraph 10 10 rand nFn_sleep
+   let testHundred =
+          runEval $ evalList rdeepseq $ testGraph 100 10 rand nFn_sleep
+   tc <- getNumCapabilities
+   workers <- initWorkers workerAction tc
+   defaultMain [bgroup "10"
+                [bench "serial" $ nfIO $ (solve serSolve) (input testTen),
+                 bench "parallel"
+                 $ nfIO $ (solve $ parSolve workers) (input testTen)],
+                bgroup "100"
+                [bench "serial" $ nfIO $ (solve serSolve) (input testHundred),
+                 bench "parallel"
+                 $ nfIO $ (solve $ parSolve workers) (input testHundred)]]
 
-                 ]]
-               {-[bgroup "Randoms"
-                [bgroup "2X2"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 2 2) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 2 2) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 2 2) (conSolveFn threadCount)],
-                 bgroup "4X4"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 4 4) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 4 4) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 4 4) (conSolveFn threadCount)],
-                 bgroup "8X8"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 8 8) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 8 8) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 8 8) (conSolveFn threadCount)],
-                 bgroup "16X16"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 16 16) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 16 16) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 16 16) (conSolveFn threadCount)],
-                 bgroup "32X32"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 32 32) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 32 32) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 32 32) (conSolveFn threadCount)],
-                 bgroup "64X64"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 64 64) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 64 64) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 64 64) (conSolveFn threadCount)],
-                 bgroup "128X128"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 128 128) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 128 128) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 128 128) (conSolveFn threadCount)],
-                 bgroup "256X256"
-                 [bench "Serial" $ nfIO
-                  $ solve (rTestInput 256 256) serSolveFn,
-                  bench "Parallel" $ nfIO
-                  $ solve (rTestInput 256 256) parSolveFn,
-                  bench "Concurrent" $ nfIO
-                  $ solve (rTestInput 256 256) (conSolveFn threadCount)]]]-}
-   where
-      testData w d = testGraph w d (mkStdGen 800815) nFn_dmesg
-      testInput w d = input sFn_dmesg $ testData w d
-      --testData w d = testGraph w d (mkStdGen 1337) nFn_randoms --fibs
-      --rTestInput w d = input sFn_randoms $ testData w d
+-- TEST FUNCTIONS
 
-
--- | Test data
+nFn_sleep :: Int -> Int -> [Int] -> Node Int
+nFn_sleep n k d = (5 + (n `mod` 3), k, d)
 
 -- | Generates a test graph. This graph will have the form of a grid of w by d
 -- nodes. The top row will have no dependencies. For all other rows, each node
@@ -345,67 +170,3 @@ testGraph w d g nFn = nodeList
       intervalList = (*) <$> [0..(d - 1)] <*> [w]
       widthList n = replicate w [n..(n + (w - 1))]
       nodeList = zipWith3 nFn lhsList midList rhsList
-
-nFn_dmesg :: Int -> Int -> [Int] -> Node Int
-nFn_dmesg = nFn_fibs
-
-sFn_dmesg :: Int -> [Int] -> IO Int
-sFn_dmesg = sFn_ioStuff $ do
-   out <- readProcess "dmesg" [] []
-   return $ length out
-
-sFn_ioStuff :: IO Int -> Int -> [Int] -> IO Int
-sFn_ioStuff a = foldM goDoStuff
-   where
-      goDoStuff 0 0 = goDoStuff 1 1
-      goDoStuff lhs 0 = goDoStuff lhs 1
-      goDoStuff 0 rhs = goDoStuff 1 rhs
-      goDoStuff lhs rhs = do
-         ioRes <- a
-         return $ (ioRes `mod` lhs) + (ioRes `mod` rhs)
-
-nFn_fibs :: Int -> Int -> [Int] -> Node Int
-nFn_fibs _ k [] = (0, k, [])
-nFn_fibs n k d = ((abs $ n `mod` length d), k, d)
-
-nFn_randoms :: Int -> Int -> [Int] -> Node Int
-nFn_randoms = nFn_fibs
-
-sFn_randoms :: Int -> [Int] -> IO Int
-sFn_randoms = foldM goRand
-   where
-      goRand 0 0 = goRand 1 1
-      goRand lhs 0 = goRand lhs 1
-      goRand 0 rhs = goRand 1 rhs
-      goRand lhs rhs = do
-         lhs' <- randomIO
-         lhs'' <- randomIO
-         rhs' <- randomIO
-         rhs'' <- randomIO
-         return $ (lhs' + lhs'' `mod` lhs) + (rhs' + rhs'' `mod` rhs)
-
-sFn_badFibs :: Int -> [Int] -> IO Int
-sFn_badFibs n s = return $ foldl' goFibs n s
-   where
-      goFibs lhs rhs = badFibs $ (lhs + rhs) `mod` 35
-
-sFn_goodFibs :: Int -> [Int] -> IO Int
-sFn_goodFibs n s = return $ foldl' goFibs n s
-   where
-      goFibs lhs rhs = goodFibs $ (lhs + rhs) `mod` 35
-
--- | Bad fibs implementation
-badFibs :: Int -> Int
-badFibs 0 = 0
-badFibs 1 = 1
-badFibs n = badFibs (n - 1) + badFibs (n - 2)
-
--- | Good fibs implementation
-goodFibs :: Int -> Int
-goodFibs 0 = 0
-goodFibs 1 = 1
-goodFibs n = goodFibs' (n, 2, 1, 0)
-   where
-      goodFibs' (to, count, n, nmo)
-         | to == count = n + nmo
-         | otherwise = goodFibs' (to, (count + 1), (n + nmo), n)
